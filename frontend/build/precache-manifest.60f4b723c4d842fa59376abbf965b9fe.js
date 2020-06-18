8022bf26e553b33ce0455f129de35297a064593a
Assia Deramchi <at.deramchi@gmail.com>
2020-06-18 15:18:08 +0100

    test deploy

---
+++ frontend/build/precache-manifest.60f4b723c4d842fa59376abbf965b9fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd49f47ef9eece534d7a0fae9f8b0461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4aeaeef1886cb293b8",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "66edd7b9e2c8ce0da567",
+    "url": "/static/css/main.35787e3f.chunk.css"
+  },
+  {
+    "revision": "6c4aeaeef1886cb293b8",
+    "url": "/static/js/2.a6de83b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6de83b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66edd7b9e2c8ce0da567",
+    "url": "/static/js/main.7d16a20f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
